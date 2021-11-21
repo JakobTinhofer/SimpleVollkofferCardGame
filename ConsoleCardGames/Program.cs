6c0d9b37889d5f6a10ddd43164fe9f7879e03d41
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2021-11-21 20:37:55 +0100

    Test commit

---
+++ ConsoleCardGames/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CardGameBase;
+
+namespace ConsoleCardGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var t = new TestClass();
+        }
+    }
+}
