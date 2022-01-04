1bba19e5e60d89c06b67b2a802bd7fed07f048d5
Andreas Tinhofer <tinhofer@tinhofer.legal>
2022-01-04 16:16:00 +0100

    Added description for Player Hand

--- VollkofferGameLibrary/PlayerHand.cs
+++ VollkofferGameLibrary/PlayerHand.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace LightBlueFox.Games.Vollkoffer
 {
+    /// <summary>
+    /// Cards in the hand of the player
+    /// </summary>
     public class PlayerHand : CardCollection
     {
     }
