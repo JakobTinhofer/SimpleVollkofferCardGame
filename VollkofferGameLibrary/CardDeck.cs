b09eaf499007dbc210a845740e5741d0b8c29b51
Simon Tinhofer <sim.tinhofer@billrothgymnasium.at>
2022-01-07 17:58:35 +0100

    Created Deck Constructors

--- VollkofferGameLibrary/CardDeck.cs
+++ VollkofferGameLibrary/CardDeck.cs
@@ -4,7 +4,39 @@ using System.Text;
 
 namespace LightBlueFox.Games.Vollkoffer
 {
+    /// <summary>
+    /// A class representing a deck of cards.
+    /// </summary>
     public class CardDeck : CardCollection
     {
+        /// <summary>
+        /// Creates a deck with one of each possible cards
+        /// </summary>
+        public CardDeck() : this(EZFilters.AllCards)
+        {
+        
+        }
+
+        /// <summary>
+        /// Creates a deck with the cards that pass the given filter.
+        /// </summary>
+        /// <param name="f">The filter for which cards to include.</param>
+        public CardDeck(CardFilter f)
+        {
+            var vals = Enum.GetValues(typeof(CardValues));
+
+            foreach (CardValues value in vals)
+            {
+                foreach (var suit in CardSuits.AllSuits)
+                {
+                 
+                    Card c = new Card(suit, value);
+                    if (f(c)) 
+                    { 
+                        Add(c); 
+                    }
+                }
+            }
+        }
     }
 }
