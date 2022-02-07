49c01388abf33c44305f39306978e8e7a14130d9
JakobTinhofer <65621944+JakobTinhofer@users.noreply.github.com>
2022-02-07 18:56:06 +0100

    Started implementing VollkofferGame

---
+++ VollkofferGameLibrary/VollkofferGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightBlueFox.Games.Vollkoffer
+{
+    public class VollkofferGame
+    {
+        private Player[] players;
+        private int round;
+        public VollkofferGame(params Player[] players)
+        {
+
+        }
+
+        private void playRound()
+        {
+
+        }
+
+
+
+    }
+}
