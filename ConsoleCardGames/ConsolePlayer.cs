7950ce14e8fd361fd1bad55baaeb04e81e6fda4c
Andreas Tinhofer <tinhofer@tinhofer.legal>
2022-02-07 18:53:49 +0100

    started imlpementing console player

---
+++ ConsoleCardGames/ConsolePlayer.cs
@@ -0,0 +1,17 @@
+﻿using LightBlueFox.Games.Vollkoffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCardGames
+{
+    internal class ConsolePlayer : Player
+    {
+        public override void DoTurn(RoundCardStack stack)
+        {
+            Console.WriteLine("It is your turn, " + Name + "!");
+        }
+    }
+}
